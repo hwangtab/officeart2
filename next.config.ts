23fd82ef10fb4062d1cb4c2cd16f3ab4fb578ecc
hwangtab <hwangtab@gmail.com>
2025-04-13 22:17:32 +0900

    Add GitHub Actions workflow and configure static export

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Add this line for static export
   images: {
     remotePatterns: [
       {
